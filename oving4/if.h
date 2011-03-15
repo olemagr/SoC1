3f3db648f0ac50cf0b4aa5500348fb929cb6186e
Ole Magnus Ruud <olemagr@gmail.com>
2011-03-15 13:58:14 +0100

    yeah

--- oving4/if.h
+++ oving4/if.h
@@ -1,21 +1,21 @@
 #ifndef   	IF_H_
 # define   	IF_H_
 
+
+// Defines
+#define ADAPTER_UPDATE_PERIOD 100
+
+
 // Interface for Button
 class button_if : public sc_interface {
 public:
   virtual void light (bool) = 0;
 };
 
-// Interface for Control, also used by bus
 class control_if : public sc_interface {
 public:
   virtual void push (int) = 0;
 };
 
-// Interface for Buss
-class bus_if : public sc_interface {
-public:
-  virtual void light (int, bool) = 0;
-};
+
 #endif 	    /* !IF_H_ */
