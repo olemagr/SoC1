c51bacde51224da622e433854cdb24c1f20d2a23
Ole Magnus Ruud <olemagr@gmail.com>
2011-03-15 15:11:37 +0100

    New constants

--- oving4/if.h
+++ oving4/if.h
@@ -3,15 +3,17 @@
 
 
 // Defines
-#define ADAPTER_UPDATE_PERIOD 100
-#define BUTTON_STATUS_ADDRESS 10
-#define FREELOC 1
-#define BUFFER_MAX_ADDRESS
+#define ADAPTER_UPDATE_PERIOD 4*100
+#define BUTTON_STATUS_ADDRESS 4*10
+#define BUTTON_STATUS_OFFSET 4*4
+#define FREELOC 4*500
+#define BUFFER_MAX_ADDRESS 4*1000
+#define CONTROL_ADDRESS 4*1
 
 struct data_packet_t {
   int packet_size = 3;
   int button_id;
-  int button_pushed = 1;
+  int button_pushed;
 };
 
 // Interface for Button
