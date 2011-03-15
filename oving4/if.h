36ea801fa1b08ed054a29e594f24f867d01815c0
Ole Magnus Ruud <olemagr@gmail.com>
2011-03-15 14:18:33 +0100

    Added data packet struct definition

--- oving4/if.h
+++ oving4/if.h
@@ -7,6 +7,12 @@
 #define BUTTON_STATUS_ADDRESS 10
 #define FREELOC 1
 
+typedef struct data_packet_t {
+  int packet_size = 3;
+  int button_id;
+  int button_pushed;
+};
+
 // Interface for Button
 class button_if : public sc_interface {
 public:
