1d274ee6b517a3dde55236b8a460c7a020170dde
Ole Magnus Ruud <olemagr@gmail.com>
2011-03-22 13:31:03 +0100

    buttondebug output

--- oving4/button.cpp
+++ oving4/button.cpp
@@ -1,6 +1,5 @@
 #include "button.h"
 
-// Construktor
 Button::Button (sc_module_name name, int _id) : sc_module (name)
 {
   SC_HAS_PROCESS (Button);
@@ -9,20 +8,26 @@ Button::Button (sc_module_name name, int _id) : sc_module (name)
   id = _id;
   lightOn = false; // Light initially off
 }
-void Button::light(bool b) {
-  if(lightOn!=b) { // Toggle light if bool different
-    lightOn=b;
-    if(b)
-      cout <<"Button" << id << ":\tLight on " << endl; // Output light change
-    else
-      cout <<"Button" << id << ":\tLight off " << endl;
-  }
+
+// Light signal response
+void Button::light(bool b) 
+{
+  if(lightOn!=b) 
+    { // Toggle light if bool different
+      lightOn=b;
+      if(b)
+	cout <<"Button " << id << ":\tLight on " << endl; // Output light change
+      else
+	cout <<"Button " << id << ":\tLight off " << endl;
+    }
 }
+
 // Called whenever button signal changes
-void Button::main(void) {
+void Button::main(void) 
+{
   if (press.read() == true)
     {
-      cout << "Button"<<id<<":\tPushed.\n";
+      cout << "Button "<<id<<":\tPushed.\n";
       bus->push(id);
     }
 }
